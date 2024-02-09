329794fe6240d28ea0b98ab0de9f8402124ac1a7
Kanishk Badola <kanishkbadola@Kanishks-MacBook-Pro.local>
2024-02-09 08:46:14 +0530

    Completed week-10 assignments

---
+++ week-10/1-postgres-simple/src/config.ts
@@ -0,0 +1,2 @@
+
+export const DB_URL = "postgresql://kanishkbadola5:SUJRGNZ46crv@ep-sweet-moon-a5atuoev.us-east-2.aws.neon.tech/neondb?sslmode=require";
---
+++ week-10/2-prisma-simple/src/db/user.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+/*
+ * Should insert into the users table
+ * Should return the User object
+ * {
+ *   username: string,
+ *   password: string,
+ *   name: string
+ * }
+ */
+export async function createUser(username: string, password: string, name: string) {
+    const newUser = await prisma.user.create({
+        data: {
+            username: username,
+            password: password,
+            name: name
+        }
+    });
+
+    return newUser;
+}
+
+/*
+ * Should return the User object
+ * {
+ *   username: string,
+ *   password: string,
+ *   name: string
+ * }
+ */
+export async function getUser(userId: number) {
+    const user = await prisma.user.findUnique({
+        where: {
+            id: userId
+        }
+    });
+
+    return user;
+}
